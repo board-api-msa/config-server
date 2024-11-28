08475a54b8aba56ca41a9a9809ef02398051909f
junbyoung96 <junbyoung96@gmail.com>
2024-11-28 19:23:48 +0900

    first commit

---
+++ src/test/java/me/junbyoung/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.junbyoung.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
